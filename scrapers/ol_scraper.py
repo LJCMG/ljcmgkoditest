51c5d91d99ac4358d5a6c7945a67bf0a27561e5b
MrBlamo <31746933+MrBlamo@users.noreply.github.com>
2017-10-03 11:25:18 -0600

    Update ol_scraper.py

--- scrapers/ol_scraper.py
+++ scrapers/ol_scraper.py
@@ -26,7 +26,7 @@ from salts_lib.constants import VIDEO_TYPES
 from salts_lib.constants import FORCE_NO_MATCH
 from salts_lib.constants import QUALITIES
 
-BASE_URL = 'http://openloadmovies.tv'
+BASE_URL = 'http://openloadmovies.org'
 Q_MAP = {'HD': QUALITIES.HD720, 'DVD': QUALITIES.HIGH, 'CAM': QUALITIES.LOW}
 
 class Scraper(scraper.Scraper):
